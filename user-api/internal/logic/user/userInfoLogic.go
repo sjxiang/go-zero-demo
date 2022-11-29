41abe5ff462d261005823b3baff6378788309eda
sjxiang <1535484943@qq.com>
2022-11-30 01:51:29 +0800

    完善请求用户信息方法

--- user-api/internal/logic/user/userInfoLogic.go
+++ user-api/internal/logic/user/userInfoLogic.go
@@ -2,9 +2,11 @@ package user
 
 import (
 	"context"
+	"errors"
 
 	"github.com/sjxiang/go-zero-demo/user-api/internal/svc"
 	"github.com/sjxiang/go-zero-demo/user-api/internal/types"
+	"github.com/sjxiang/go-zero-demo/user-api/model"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -24,7 +26,18 @@ func NewUserInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserInfo
 }
 
 func (l *UserInfoLogic) UserInfo(req *types.UserInfoReq) (resp *types.UserInfoResp, err error) {
-	// todo: add your logic here and delete this line
+	
+	user, err := l.svcCtx.UserModel.FindOne(l.ctx, req.UserId)
+	if err != nil && err != model.ErrNotFound {
+		return nil, errors.New("查询数据失败")
+	}
+	if user == nil {
+		return nil, errors.New("用户不存在")
+	}
 
-	return
+	return &types.UserInfoResp{ 
+		UserId: user.Id,
+		NickName: user.Nickname,
+	}, nil
+
 }
